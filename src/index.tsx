77f82e19f45a9f603eb829b111a7cfdc877b0bbb
Alexnader Shcherbakov <alexander@sugarwork.com>
2025-01-18 22:40:50 +0100

    create gh deploy script

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+// Get the base name from the homepage in package.json
+const basename = process.env.PUBLIC_URL;
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
